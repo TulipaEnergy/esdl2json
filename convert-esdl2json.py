d414909460163a0c87e089297dec2e620ba78402
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2024-01-17 17:02:30 +0100

    Workaround pyecore to use custom JSON encoder

--- convert-esdl2json.py
+++ convert-esdl2json.py
@@ -1,16 +1,46 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
+from datetime import datetime
+import json
 
 from esdl.esdl_handler import EnergySystemHandler
 from pyecore.resources.json import JsonResource
+
+
+class JSONEncoderDT(json.JSONEncoder):
+    def default(self, obj):
+        match obj:
+            case datetime():
+                return obj.isoformat()
+        return super().default(obj)
+
+
+class JsonResourceDT(JsonResource):
+    """FIXME: hack to serialise datetime objects by overriding save"""
+
+    def save(self, output=None, options=None):
+        self.options = options or {}
+        stream = self.open_out_stream(output)
+        dict_list = []
+        for root in self.contents:
+            dict_list.append(self.to_dict(root))
+        if len(dict_list) <= 1:
+            dict_list = dict_list[0]
+
+        stream.write(
+            json.dumps(dict_list, indent=self.indent, cls=JSONEncoderDT).encode("utf-8")
+        )
+        stream.flush()
+        self.uri.close_stream()
+        self.options = None
 
 
 def convert(esdl_file: str):
     esh = EnergySystemHandler()
     esh.create_empty_energy_system(name="Test")
     esh.load_file(esdl_file)
-    esh.rset.resource_factory["json"] = lambda uri: JsonResource(uri)
+    esh.rset.resource_factory["json"] = lambda uri: JsonResourceDT(uri)
 
     json_file = esdl_file.replace(".esdl", ".json")
     esh.save(json_file)
